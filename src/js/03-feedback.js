c72557ec93f15ddf1c78a6c08fcb5fd77f464ab8
Alexei Solotvinskiy <97736656+alexvile@users.noreply.github.com>
2022-05-25 22:02:04 +0300

    solved 3 task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,50 @@
+import throttle from "lodash.throttle";
+
+const formEl = document.querySelector('.feedback-form');
+const emailInput = document.querySelector("[name=email]");
+const feedbackInput = document.querySelector("[name=message]");
+
+const STORAGE_KEY = "feedback-form-state";
+
+
+formEl.addEventListener('submit', onFormSubmit);
+formEl.addEventListener('input', throttle(onFormInput, 500));
+
+
+
+populateTextArea();
+
+
+function onFormInput(e) {
+    // console.log(e.target);
+    // console.log(e.currentTarget);
+
+    const emailValue = emailInput.value;
+    const feedbackValue = feedbackInput.value;
+
+    localStorage.setItem(STORAGE_KEY, JSON.stringify({email: emailValue, message: feedbackValue}))
+ };
+
+
+
+function onFormSubmit(e) {
+    e.preventDefault();
+
+    e.currentTarget.reset();
+
+    console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+
+    localStorage.removeItem(STORAGE_KEY);
+};
+
+ 
+function populateTextArea() {
+    const savedMessage = localStorage.getItem(STORAGE_KEY);
+    const parsedMessage = JSON.parse(savedMessage);
+
+    if (savedMessage) {
+        emailInput.value = parsedMessage.email;
+        feedbackInput.value = parsedMessage.message;
+    }
+}
 
