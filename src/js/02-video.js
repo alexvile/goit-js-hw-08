5f34a67302511ea106e6a4ee1825878193dee294
Alexei Solotvinskiy <97736656+alexvile@users.noreply.github.com>
2022-05-25 20:50:44 +0300

    solved 2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,54 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+
+
+const onPlay = function () {
+ 
+    player.getCurrentTime().then(function (seconds) {
+        // console.log(seconds);
+        localStorage.setItem('videoplayer-current-time', seconds);
+    }).catch(function (error) {
+        // console.log(error);
+    });
+
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+
+
+// console.log(localStorage);
+// console.log(localStorage.getItem('videoplayer-current-time'));
+
+function populateCurrentTime() {
+    const savedTime = localStorage.getItem('videoplayer-current-time');
+
+    if (savedTime) {
+    
+        player.setCurrentTime(savedTime).then(function(seconds) {
+            // seconds = the actual time that the player seeked to
+        }).catch(function(error) {
+            switch (error.name) {
+                case 'RangeError':
+                    // the time was less than 0 or greater than the video’s duration
+                    break;
+
+                default:
+                    // some other error occurred
+                    break;
+            }
+        });
+    }
+}
+
+populateCurrentTime();
+
+
+
+
 
